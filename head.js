0fb133286e8e8c3c185a871a10e63ddcd7e14b2e
Olivia Wong <olivia.wy.wong@gmail.com>
2021-07-22 22:05:04 +0000

    modified assertEqual; head submission; tail submission

--- head.js
+++ head.js
@@ -8,6 +8,13 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(array) {
+  return array[0];
+};
+
+
+
 //TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 2);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
