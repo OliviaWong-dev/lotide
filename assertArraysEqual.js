9cd51be8b4b07cd48d05f330e8794317aec789ec
Olivia Wong <olivia.wy.wong@gmail.com>
2021-07-24 21:51:58 +0000

    first draft

---
+++ assertArraysEqual.js
@@ -0,0 +1,39 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+
+const eqArrays = function(actualArr, expectedArr) {
+  if (actualArr.length != expectedArr.length) {
+    return false;
+  }
+  for (let i = 0; i < actualArr.length; i++) {
+    if (actualArr[i] !== expectedArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should PASS
