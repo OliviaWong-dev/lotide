56e0cc8cb6ede4b826ce84f51c579271771cb233
Olivia Wong <olivia.wy.wong@gmail.com>
2021-07-24 19:42:51 +0000

    first draft

---
+++ eqArray.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+
+
+const eqArrays = function(actualArr, expectedArr) {
+  let result;
+  for (actualList of actualArr) {
+    for (expectedList of expectedArr) {
+      if (actualList === expectedList) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
