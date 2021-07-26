7ada099a3df697fe057b70e674e6bce5cd1f88bf
Olivia Wong <olivia.wy.wong@gmail.com>
2021-07-26 06:49:50 +0000

    first draft

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const countLetters = function(word) {
+  let count = {};
+  for (letter of word) {
+    if (!count[letter]) {
+      count[letter] = 1;
+    } else {
+      count[letter] = count[letter] + 1;
+    }
+  }
+  return count;
+};
+
+
+assertEqual(countLetters("hello").h, 1);
+assertEqual(countLetters("hello").e, 1);
+assertEqual(countLetters("hello").l, 2);
+assertEqual(countLetters("hello").o, 1);
+
