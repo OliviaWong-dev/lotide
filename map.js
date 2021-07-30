476d82ce64ab038b054c47427822da2af2f15fcf
Olivia Wong <olivia.wy.wong@gmail.com>
2021-07-30 23:10:26 +0000

    first draft

--- map.js
+++ map.js
@@ -2,14 +2,14 @@
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
-  return results
-}
+  return results;
+};
 
 
 const eqArrays = function(actualArr, expectedArr) {
-  if (actualArr.length != expectedArr.length) {
+  if (actualArr.length !== expectedArr.length) {
     return false;
   }
   for (let i = 0; i < actualArr.length; i++) {
@@ -21,7 +21,7 @@ const eqArrays = function(actualArr, expectedArr) {
 };
 
 
-const assertArraysEqual = function(actual, expected) { 
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -32,7 +32,7 @@ const assertArraysEqual = function(actual, expected) {
 
 const words = ["ground", "control", "to", "major", "tom"];
 
-assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"])
-assertArraysEqual(map(words, word => "hello " + word), ["hello ground","hello control","hello to","hello major","hello tom"])
-assertArraysEqual(map(words, word => word.toUpperCase()), ["GROUND","CONTROL","TO","MAJOR","TOM"])
-assertArraysEqual(map(words, word => word.length), [6,7,2,5,3])
+assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]);
+assertArraysEqual(map(words, word => "hello " + word), ["hello ground","hello control","hello to","hello major","hello tom"]);
+assertArraysEqual(map(words, word => word.toUpperCase()), ["GROUND","CONTROL","TO","MAJOR","TOM"]);
+assertArraysEqual(map(words, word => word.length), [6,7,2,5,3]);
