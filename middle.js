85c423c4835102a179659d2e58b702fb48321968
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-09 18:35:50 +0000

    refactor updated

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(actualArr, expectedArr) {
-  if (actualArr.length !== expectedArr.length) {
-    return false;
-  }
-  for (let i = 0; i < actualArr.length; i++) {
-    if (actualArr[i] !== expectedArr[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
 const middle = function(array) {
   let result;
   if (array.length > 2 && array.length % 2 !== 0) {
@@ -32,6 +11,4 @@ const middle = function(array) {
 };
 
 
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
