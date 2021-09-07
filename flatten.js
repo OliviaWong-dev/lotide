7aa12a50719c00509feea702ae5fbc87351255aa
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-07 02:18:04 +0000

    removed testing code and unnecessary code

--- flatten.js
+++ flatten.js
@@ -1,25 +1,4 @@
-const eqArrays = function(actualArr, expectedArr) {
-  if (actualArr.length !== expectedArr.length) {
-    return false;
-  }
-  for (let i = 0; i < actualArr.length; i++) {
-    if (actualArr[i] !== expectedArr[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
-const flatten = function(array) {
+const flatten = function (array) {
   let newArr = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i]) === true) {
@@ -27,16 +6,11 @@ const flatten = function(array) {
       for (let j = 0; j < array[i].length; j++) {
         newArr.push(array[i][j]);
       }
-    } else {;
+    } else {
       newArr.push(array[i]);
     }
   }
   return newArr;
 };
-
-
-console.log(flatten([1, 2, [3, 4], 5, [6]])); //[1, 2, 3, 4, 5, 6])
-
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
 
 module.exports = flatten;
