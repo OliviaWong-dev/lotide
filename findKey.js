2003d1410f1803e799abc0324170c0641ef72f8c
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-07 02:12:21 +0000

    removed testing code

--- findKey.js
+++ findKey.js
@@ -1,14 +1,5 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
-const findKey = function(object, callback) {
-  let result = '';
+const findKey = function (object, callback) {
+  let result = "";
   let keys = Object.keys(object);
   for (key of keys) {
     if (callback(object[key])) {
@@ -18,34 +9,5 @@ const findKey = function(object, callback) {
   }
   return result;
 };
-
-
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 2), "noma");
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 3), "Akaleri");
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 1), "Blue Hill");
 
 module.exports = findKey;
