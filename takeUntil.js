735af78736c35d5281851dae9f2453d8cf584302
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-07 16:01:44 +0000

    removed testing functions and codes

--- takeUntil.js
+++ takeUntil.js
@@ -10,39 +10,4 @@ const takeUntil = function (array, callback) {
   return result;
 };
 
-
-const eqArrays = function (actualArr, expectedArr) {
-  if (actualArr.length !== expectedArr.length) {
-    return false;
-  }
-  for (let i = 0; i < actualArr.length; i++) {
-    if (actualArr[i] !== expectedArr[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`💓💓💓Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
-
 module.exports = takeUntil;
