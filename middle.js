df91d266e4373ab171158c1f289f8d35c66e71f5
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-07 15:57:49 +0000

    add code to return empty array for arrays with 1 or 2 elements

--- middle.js
+++ middle.js
@@ -1,13 +1,22 @@
 const middle = function (array) {
   let result;
-  if (array.length > 2 && array.length % 2 !== 0) {
+  if (array.length < 3) {
+    return [];
+  }
+  if (array.length % 2 !== 0) {
     const indexofMiddle = (array.length - 1) / 2;
     result = [array[indexofMiddle]];
-  } else if (array.length > 2 && array.length % 2 === 0) {
+  }
+  if (array.length % 2 === 0) {
     const indexofMiddle = array.length / 2;
     result = [array[indexofMiddle - 1], array[indexofMiddle]];
   }
   return result;
 };
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log(middle([0])); // => [3, 4]
+console.log(middle([0, 1])); // => [3, 4]
 
 module.exports = middle;
